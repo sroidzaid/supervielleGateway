bfbb42ac78e3f8459b224d4b60df66ea0ce4a9f3
Sebas <sebastian.roidzaid@gmail.com>
2021-07-07 17:58:51 -0300

    Commit Gateway

---
+++ src/main/java/com/supervielleGateway/SupervielleGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.supervielleGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervielleGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupervielleGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supervielleGateway/SupervielleGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supervielleGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervielleGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
